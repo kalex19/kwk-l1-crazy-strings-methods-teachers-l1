9ac3131b30cdfc791312bbf802e30d1bc870a152
kalex19 <klewis@rockymountainprep.org>
2018-08-11 12:19:27 +0000

    Done.

--- crazy_string.rb
+++ crazy_string.rb
@@ -1,5 +1,8 @@
 # write your method here
 
-def crazy_strings(a,b)
-
+def crazy_strings(name,word)
+  puts ("#{name}".upcase.reverse)
+  puts ("#{word}".swapcase)
 end
+
+crazy_strings("hello","Bob")
